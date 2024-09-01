a972384dee259c913aac51c6d4b9efd2a9b24dce
Donovan Broquin <donovan.broquin@outlook.com>
2024-09-01 19:33:49 +0200

    chore: remove Terraform comments

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -1,19 +1,3 @@
-# resource "ovh_domain_zone_record" "iut_ipv4" {
-#   zone      = var.dns_zone
-#   subdomain = "iut"
-#   fieldtype = "A"
-#   ttl       = 300
-#   target    = hcloud_server.main.ipv4_address
-# }
-
-# resource "ovh_domain_zone_record" "iut_ipv6" {
-#   zone      = var.dns_zone
-#   subdomain = "iut"
-#   fieldtype = "AAAA"
-#   ttl       = 300
-#   target    = hcloud_server.main.ipv6_address
-# }
-
 resource "ovh_domain_zone_record" "ipv4" {
   for_each  = toset(var.subdomains)
   
